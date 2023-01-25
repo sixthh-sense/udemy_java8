524ed88e0e031ecfdb3bc0016c49be2f969212f8
haein.sung <blue6zombie6@gmail.com>
2023-01-25 11:18:07 +0900

    lab #5 - bonus lab progressing, java.lang.ArithmaticException (outisde range: byte and int)

--- src/main/java/com/acme/testing/TestConversionService.java
+++ src/main/java/com/acme/testing/TestConversionService.java
@@ -2,6 +2,8 @@ package com.acme.testing;
 
 import com.acme.utils.ConversionService;
 
+import java.math.BigDecimal;
+
 public class TestConversionService {
 
 	/**
@@ -10,20 +12,20 @@ public class TestConversionService {
 	public static void main(String[] args) {
 		System.out.println(ConversionService.fluidOunces(1.1f));
 		System.out.println(ConversionService.gallons(2.2f));
-		System.out.println(ConversionService.grams(30));
+		System.out.println(ConversionService.grams(BigDecimal.valueOf(30)));
 		System.out.println(ConversionService.milliliters(40));
-		System.out.println(ConversionService.ounces(50));
+		System.out.println(ConversionService.ounces(BigDecimal.valueOf(50)));
 		System.out.println(ConversionService.pints(6.6f));
-		System.out.println(ConversionService.pounds(7.7));
+		System.out.println(ConversionService.pounds(BigDecimal.valueOf(7.7)));
 
 		// can you figure out why these do not compile
 		ConversionService.fluidOunces(1.1f); // 1.1 -> 1.1f :: double -> float
-		ConversionService.grams(30); //  30L -> 30 :: long -> int
+		ConversionService.grams(BigDecimal.valueOf(30)); //  30L -> 30 :: long -> int
 		ConversionService.milliliters((int) 4.0); // 4.0 -> (int) 4.0 or 4? :: double -> int
-		short grams = (short) ConversionService.grams(30); // short grams -> int grams :: short -> int // can't decide what part i should change
-		byte byteGrams = (byte) ConversionService.grams(30); // byte byteGrams -> int byteGrams :: byte -> int // can't decide what part i should change
+		short grams = ConversionService.grams(BigDecimal.valueOf(30)).shortValueExact(); // short grams -> int grams :: short -> int // can't decide what part i should change
+		// byte byteGrams = ConversionService.grams(BigDecimal.valueOf(30)).byteValueExact(); // byte byteGrams -> int byteGrams :: byte -> int // can't decide what part i should change
 		System.out.println(grams);
-		System.out.println(byteGrams);
+		// System.out.println(byteGrams);
 
 		// why do these still work even though the types are different
 		// small range -> big range
@@ -36,15 +38,15 @@ public class TestConversionService {
 		double decimalMilliliters = ConversionService.milliliters(40); // int to double
 		System.out.println(decimalMilliliters);
 		short s = 30;
-		System.out.println(ConversionService.grams(s)); // short to int
+		System.out.println(ConversionService.grams(BigDecimal.valueOf(s))); // short to int
 		byte b = 4;
 		System.out.println(ConversionService.milliliters(b)); // byte to int
 		char z = 'z';
 		System.out.println(ConversionService.milliliters(z)); // char to int(get that unicode something)
 		System.out.println(ConversionService.gallons(200)); // int to float
-		System.out.println(ConversionService.ounces(50.5f)); // float to double
+		System.out.println(ConversionService.ounces(BigDecimal.valueOf(50.5f))); // float to double
 		System.out.println(ConversionService.pints(6L)); // long to float
-		System.out.println(ConversionService.pounds(7L)); // long to double
+		System.out.println(ConversionService.pounds(BigDecimal.valueOf(7L))); // long to double
 
 		// compare these results.  Can you tell why they are different?
 		// in the small world(range), when things get out of range, they get chunked(precision loss)
@@ -55,8 +57,8 @@ public class TestConversionService {
 		System.out.println(bigGallons2);
 
 		System.out.println("------------------------------------");
-		int bigGrams = ConversionService.grams(1234567890);
-		System.out.println(bigGrams);
+//		int bigGrams = ConversionService.grams(BigDecimal.valueOf(1234567890)).intValueExact();
+//		System.out.println(bigGrams);
 		long bigGrams2 = 1234567890L * 1000L;
 		System.out.println(bigGrams2);
 	}
--- src/main/java/com/acme/utils/ConversionService.java
+++ src/main/java/com/acme/utils/ConversionService.java
@@ -1,12 +1,14 @@
 package com.acme.utils;
 
+import java.math.BigDecimal;
+
 public class ConversionService {
     /**
      *  weight conversion rates
      * */
-    public static double kilogramToPounds = 2.2046;
-    public static int kilogramToGrams = 1000;
-    public static double kilogramToOunces = 35.274;
+    public static BigDecimal kilogramToPounds = new BigDecimal("2.2046"); // double to BigDecimal
+    public static BigDecimal kilogramToGrams = new BigDecimal(1000); // int to BigDecimal
+    public static BigDecimal kilogramToOunces = new BigDecimal("35.274"); // double to BigDecimal
 
     /**
      *  volume conversion rates
@@ -16,14 +18,14 @@ public class ConversionService {
     public static float literToPints = 2.1134f;
     public static short literToMilliliters = 1_000;
 
-    public static double pounds(double kilograms) {
-        return kilograms * kilogramToPounds;
+    public static BigDecimal pounds(BigDecimal kilograms) {
+        return kilograms.multiply(kilogramToPounds);
     }
-    public static int grams(int kilograms) {
-        return kilograms * kilogramToGrams;
+    public static BigDecimal grams(BigDecimal kilograms) {
+        return kilograms.multiply(kilogramToGrams);
     }
-    public static double ounces(double kilograms) {
-        return kilograms * kilogramToOunces;
+    public static BigDecimal ounces(BigDecimal kilograms) {
+        return kilograms.multiply(kilogramToOunces);
     }
     public static float fluidOunces(float liters) {
         return liters * literToFluidOunce;
