d25c12322ca41fa05cf08a3bc781909108493140
haein.sung <blue6zombie6@gmail.com>
2023-01-25 10:42:21 +0900

    ConversionService.java - updated for lab #5 primitives

--- src/main/java/com/acme/utils/ConversionService.java
+++ src/main/java/com/acme/utils/ConversionService.java
@@ -1,6 +1,47 @@
 package com.acme.utils;
 
 public class ConversionService {
+    /**
+     *  weight conversion rates
+     * */
+    public static double kilogramToPounds = 2.2046;
+    public static int kilogramToGrams = 1000;
+    public static double kilogramToOunces = 35.274;
+
+    /**
+     *  volume conversion rates
+     */
+    public static float literToFluidOunce = 33.814f;
+    public static float literToGallon = 0.2642f;
+    public static float literToPints = 2.1134f;
+    public static short literToMilliliters = 1_000;
+
+    public static double pounds(double kilograms) {
+        return kilograms * kilogramToPounds;
+    }
+    public static int grams(int kilograms) {
+        return kilograms * kilogramToGrams;
+    }
+    public static double ounces(double kilograms) {
+        return kilograms * kilogramToOunces;
+    }
+    public static float fluidOunces(float liters) {
+        return liters * literToFluidOunce;
+    }
+
+    public static float gallons(float liters) {
+        return liters * literToGallon;
+    }
+    public static float pints(float liters) {
+        return liters * literToPints;
+    }
+    public static int milliliters(int liters) {
+        return liters * literToMilliliters;
+    }
+
+
+
+    /*
     public static boolean fluidOunces(float v) {
         return false;
     }
@@ -28,4 +69,5 @@ public class ConversionService {
     public static boolean pounds(double v) {
         return false;
     }
+     */
 }
