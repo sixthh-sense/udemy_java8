87c3cf6c05c8d8fc8de9e946d260d6502d00f231
haein.sung <blue6zombie6@gmail.com>
2023-01-27 10:51:14 +0900

    lap 9 step 1 complete

---
+++ src/main/java/com/acme/domain/Good.java
@@ -0,0 +1,87 @@
+package com.acme.domain;
+
+public class Good {
+    public enum UnitOfMeasureType {
+        LITER, GALLON, CUBIC_METER, CUBIC_FEET
+    }
+
+    private String name;
+    private int modelNumber;
+    private double height;
+    private UnitOfMeasureType unitOfMeasure;
+    private boolean flammable = true;
+    private double weightPerUofM;
+
+    // getter & setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(int modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public UnitOfMeasureType getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasureType unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
+    public boolean isFlammable() {
+        return flammable;
+    }
+
+    public void setFlammable(boolean flammable) {
+        this.flammable = flammable;
+    }
+
+    public double getWeightPerUofM() {
+        return weightPerUofM;
+    }
+
+    public void setWeightPerUofM(double weightPerUofM) {
+        this.weightPerUofM = weightPerUofM;
+    }
+
+     // constructors
+    public Good(String name, int modelNumber, double height, UnitOfMeasureType uoM, boolean flammable, double wgtPerUoM) {
+        this.name = name;
+        this.modelNumber = modelNumber;
+        this.height = height;
+        this.unitOfMeasure = uoM;
+        this.flammable = flammable;
+        this.weightPerUofM = wgtPerUoM;
+    }
+
+    public String toString() {
+        return name + "-" + modelNumber;
+    }
+
+    public double volume() {
+        return 0.0;
+    }
+
+    public double weight() {
+        return volume() * weightPerUofM;
+    }
+
+
+}
