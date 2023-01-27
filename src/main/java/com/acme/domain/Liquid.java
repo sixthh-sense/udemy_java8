6916c97c0f072ef7f9c5a98db746b08704927974
haein.sung <blue6zombie6@gmail.com>
2023-01-27 10:54:56 +0900

    lap 9 step 2 complete

---
+++ src/main/java/com/acme/domain/Liquid.java
@@ -0,0 +1,28 @@
+package com.acme.domain;
+
+public class Liquid extends  Good{
+
+    private double radius;
+
+    // getter & setter
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public Liquid(String name, int modelNumber, double height, UnitOfMeasureType uoM, boolean flammable, double wgtPerUoM, double radius) {
+        super(name, modelNumber, height, uoM, flammable, wgtPerUoM);
+        this.radius = radius;
+    }
+
+    public double volume() {
+        return Math.PI * radius * radius * getHeight();
+    }
+
+    public String toString() {
+        return super.toString() + " (liquid) " + volume() + " " + getUnitOfMeasure();
+    }
+}
