dbfb9957c6b087b02c07d69affbd930c533abf26
haein.sung <blue6zombie6@gmail.com>
2023-01-27 10:59:37 +0900

    lap 9 step 3 and 4 complete

--- src/main/java/com/acme/testing/TestGoods.java
+++ src/main/java/com/acme/testing/TestGoods.java
@@ -1,4 +1,9 @@
 package com.acme.testing;
+
+import com.acme.domain.Good;
+import com.acme.domain.Liquid;
+import com.acme.domain.Solid;
+
 /*
 import com.acme.domain.Good;
 import com.acme.domain.Liquid;
@@ -8,12 +13,12 @@ import com.acme.domain.Good.UnitOfMeasureType;
 public class TestGoods {
 
 	public static void main(String[] args) {
-		/*
-		Liquid glue = new Liquid("Acme Glue", 2334, 4, UnitOfMeasureType.LITER,
+
+		Liquid glue = new Liquid("Acme Glue", 2334, 4, Good.UnitOfMeasureType.LITER,
 				false, 15, 6);
 		Liquid paint = new Liquid("Acme Invisible Paint", 2490, 0.65,
-				UnitOfMeasureType.GALLON, true, 0.70, 12);
-		Solid anvil = new Solid("Acme Anvil", 1668, 0.3, UnitOfMeasureType.CUBIC_METER, false, 5000, 0.5,
+				Good.UnitOfMeasureType.GALLON, true, 0.70, 12);
+		Solid anvil = new Solid("Acme Anvil", 1668, 0.3, Good.UnitOfMeasureType.CUBIC_METER, false, 5000, 0.5,
 				0.5);
 
 		System.out.println(glue);
@@ -29,6 +34,5 @@ public class TestGoods {
 		x = paint;
 		System.out.println("Is " + x + " flammable?  " + x.isFlammable());
 
-		 */
 	}
 }
